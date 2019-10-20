90512ad7e0e62b928ed1e7f0aab7434821f22836
Pucado <40542114+NotaNewbieCoder@users.noreply.github.com>
2019-10-20 00:47:07 -0700

    Update nairalandscraper.py

--- nairalandscraper.py
+++ nairalandscraper.py
@@ -1,26 +1,23 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[4]:
-
-
-
+"""importing libraries"""
+import urllib
 import requests
 from bs4 import BeautifulSoup
-import matplotlib.pyplot as plt
-import urllib
 
-WordList = []
+WORDLIST = []
+
 
 def put_in_file():
-    global WordList
-    file_name = searchTerm + '.txt'
-    for word in WordList:
+    """save searched comments as txt file"""
+    file_name = SEARCHTERM + '.txt'
+    for word in WORDLIST:
         with open(file_name, "a+", encoding="utf-8") as f:
             f.write(word)
 
 
 def word_count(string):
+    """count number of words in sentence and return count"""
     counts = dict()
     words = string.split()
 
@@ -29,45 +26,45 @@ def word_count(string):
             counts[word] += 1
         else:
             counts[word] = 1
-
     return len(counts)
 
 
-def search_item(search_term, next=False, page=0,  board=0):
+def search_item(search_term, next=False, page=0, board=0):
+    """search for inputted text on nairaland"""
     if next == False:
-        page = requests.get("https://www.nairaland.com/search?q=" + urllib.parse.quote_plus(str(search_term)) + "&board="+str(board))
+        page = requests.get("https://www.nairaland.com/search?q="
+                            + urllib.parse.quote_plus(str(search_term))
+                            + "&board="+str(board))
     else:
         page = requests.get("https://www.nairaland.com/search/"
-                            + str(search_term) + "/0/"+str(board)+"/0/1" + str(page))
+                            + str(search_term) + "/0/"+str(board)
+                            +"/0/1" + str(page))
     soup = BeautifulSoup(page.content, 'html.parser')
-
     comments = soup.findAll("div", {"class": "narrow"})
-
     return comments
 
 
 def add_to_word_list(strings):
-    global WordList
+    """adding each comment to Word list"""
     k = 0
     while k < len(strings):
         if word_count(strings[k].text) > 1:
-            WordList.append(strings[k].text)
+            WORDLIST.append(strings[k].text)
         k += 1
 
 
-searchTerm = input("Enter search term: ")
-board = int(input("Enter Section: "))
+SEARCHTERM = input("Enter search term: ")
+BOARD = int(input("Enter Section: "))
 
-j = 0
+J = 0
 
-while j < 20:
-    if j == 0:
-        nextItem = False
+while J < 20:
+    if J == 0:
+        NEXTITEM = False
     else:
-        nextItem = True
-    commentsCurrent = search_item(searchTerm, nextItem, j,  board)
-    add_to_word_list(commentsCurrent)
-    j += 1
+        NEXTITEM = True
+    COMMENTSCURRENT = search_item(SEARCHTERM, NEXTITEM, J, BOARD)
+    add_to_word_list(COMMENTSCURRENT)
+    J += 1
 
 put_in_file()
-
